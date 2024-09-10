dde6d8877c902acea212dc04b956dd5a7a9a92cf
abdul-kadir1 <abdulkadir80949@gmail.com>
2024-09-10 20:27:25 +0530

    in this file adding decresing order code of quick sort

--- Quick_sort.java
+++ Quick_sort.java
@@ -1,4 +1,4 @@
-public class Quick_sort {
+class Quick_sort {
   // displaying array
     static void displayArr(int [] arr){
       for(int val : arr){
@@ -65,3 +65,65 @@ public class Quick_sort {
   }
 
 }
+
+
+// quick sort in decreasing order--------------------
+
+
+ class Quick_sort_decresing {
+  // Displaying array
+  static void displayArr(int[] arr) {
+      for (int val : arr) {
+          System.out.print(val + " ");
+      }
+  }
+
+  // Swapping method
+  static void swap(int arr[], int x, int y) {
+      int temp = arr[x];
+      arr[x] = arr[y];
+      arr[y] = temp;
+  }
+
+  // Partition method for descending order
+  static int partition(int arr[], int st, int end) {
+      int pivot = arr[st];
+      int i = st + 1;
+      int j = end;
+
+      while (i <= j) {
+          while (i <= end && arr[i] > pivot) i++;
+          while (j >= st + 1 && arr[j] < pivot) j--;
+
+          if (i < j) {
+              swap(arr, i, j);
+          }
+      }
+      swap(arr, st, j);
+      return j;
+  }
+
+  // Quicksort method
+  static void quicksort(int[] arr, int st, int end) {
+      // Base Case
+      if (st >= end) return;
+      int pi = partition(arr, st, end);
+      quicksort(arr, st, pi - 1);
+      quicksort(arr, pi + 1, end);
+  }
+
+  public static void main(String[] args) {
+      int arr[] = {1,2,3,4,5};
+
+      System.out.println("Array before sorting:");
+      displayArr(arr);
+      System.out.println();
+
+      quicksort(arr, 0, arr.length - 1);
+
+      System.out.println("Array after sorting:");
+      displayArr(arr);
+  }
+}
+
+
